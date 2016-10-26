79eead7c77f87a6d7d6dd47d575ede7f474a5c11
Harrington Joseph <hello@hjoseph.com>
2016-10-26 12:07:25 -0700

    Removed package data and added templates to manifest

--- setup.py
+++ setup.py
@@ -13,21 +13,9 @@ test_requirements = [
     # TODO: put package test requirements here
 ]
 
-
-base_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                            'blueflask'))
-templates_path = os.path.join(base_path, 'templates')
-package_data = []
-
-for topdir, _, files in os.walk(templates_path):
-    for f in files:
-        if f.startswith('.') or f.endswith('.pyc'):
-            continue
-        package_data.append(os.path.join(topdir, f))
-
 setup(
     name='blueflask',
-    version='0.1.3',
+    version='0.1.4',
     description="Flask boilerplate to create an application with the idea of pluggable blueprints.",
     author="Harrington Joseph",
     author_email='hello@hjoseph.com',
@@ -37,9 +25,6 @@ setup(
     ],
     package_dir={'blueflask':
                  'blueflask'},
-    package_data={
-        'blueflask': package_data
-    },
     entry_points={
         'console_scripts': [
             'blueflask=blueflask.cli:main'
